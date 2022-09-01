82add0fa37e18e18f34601d843425a610ffdcee0
C:/Users/Haris Niazi <muhammadharisniaxi@gmail.com>
2022-09-01 12:04:18 +0500

    CalCulatorInProgress

---
+++ PythonPractice/SalaryCalculator.py
@@ -0,0 +1,8 @@
+
+print("")
+print("---------------------------------------------------------------------")
+print("                   Hello!Welcome to Salary Calculator"                )
+print("---------------------------------------------------------------------")
+print("")
+Slaray=int(input("Enter Employee Salary: "))
+print("")
