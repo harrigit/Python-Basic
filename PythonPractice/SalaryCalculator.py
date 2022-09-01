89b51d3a17ab5c4b3adca7be9b55d29a4ff8a670
C:/Users/Haris Niazi <muhammadharisniaxi@gmail.com>
2022-09-01 13:10:30 +0500

    LitteImplemented

--- PythonPractice/SalaryCalculator.py
+++ PythonPractice/SalaryCalculator.py
@@ -4,5 +4,24 @@ print("---------------------------------------------------------------------")
 print("                   Hello!Welcome to Salary Calculator"                )
 print("---------------------------------------------------------------------")
 print("")
-Slaray=int(input("Enter Employee Salary: "))
+Slaray=int(input("Enter Employee Salary of Month: "))
+WorkingHours=int(input("Enter total working Hours In a Month: "))
+print("")
+if WorkingHours>176:
+    print("Aba Sae Dal working Hours and Must Not greater Than 176 ")
+elif Slaray==110000:
+    perday=Slaray/22
+    perhour=perday/8
+    Total=WorkingHours*perhour
+    print("Total Salary Afer calculation is= ",Total)
+elif Slaray==40000:
+    perday=Slaray/22
+    perhour=perday/8
+    Total=WorkingHours*perhour
+    print("Total Salary Afer calculation is= ",Total)
+else:
+    print("Abhi baki employees ki Salary ka mujha Ni pta ")
+ 
+print("")  
+print("---------------------The End----------------------")
 print("")
